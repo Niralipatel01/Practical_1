9cfc27caaab13ceea1c968d3a66ad72576773cd6
Student <niralip1810@gmail.com>
2022-08-10 10:41:49 +0530

    N1

---
+++ src/main/kotlin/p1_2.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val a: Int = 22
+    val b: Double = a.toDouble()
+    val c: String = "10"
+    val d: Int = c.toInt()
+
+    val e: Double = b.toDouble()
+
+    println("Integer value:"+a)
+    println("Double value:"+b)
+    println("String value:"+c)
+    println("Integer value:"+d)
+    println("Double value:"+e)
+}
