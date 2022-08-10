0978a2f75fa23028fceb9f8aa19000a50e631421
Student <niralip1810@gmail.com>
2022-08-10 10:49:36 +0530

    N1

---
+++ src/main/kotlin/p1_3.kt
@@ -0,0 +1,13 @@
+fun main(){
+    print("Enter your name:")
+    var a = readLine()
+    print("Enter your enrollment no:")
+    var b = readLine()
+    print("Enter your branch:")
+    var c = readLine()
+    print("Enter your class:")
+    var d = readLine()
+    print("Enter your Batch:")
+    var e = readLine()
+    print("Enter ")
+}
