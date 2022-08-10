a3f3afb70ced7eceee382b7082f6d8d5c62428f0
Student <niralip1810@gmail.com>
2022-08-10 10:23:35 +0530

    N1

---
+++ src/main/kotlin/p1_1.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val a: Int = 22
+    val b: Float = 1.5f
+    val c: Char = 'T'
+    val d: String = "nirali"
+    val e: Boolean = true
+    val f: Double = 1.58
+    val g: Long = 15836274019283746L
+    val h: Short = -234
+    val i: Byte = 127
+
+    println("Integer value:"+a)
+    println("Float value:"+b)
+    println("Character value:"+c)
+    println("String value:"+d)
+    println("Boolean value:"+e)
+    println("Double value:"+f)
+    println("Long value:"+g)
+    println("Short value:"+h)
+    println("Byte value:"+i)
+
+
+}
